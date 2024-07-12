ff7806bb3d4ded53fdba4467521dfdf9de3683e8
dm92pl <dm92pl@yahoo.com>
2024-07-12 19:47:24 +0100

    Update time_series_visualizer.py

--- time_series_visualizer/time_series_visualizer.py
+++ time_series_visualizer/time_series_visualizer.py
@@ -2,10 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the dataset
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
-# Clean the data
 df = df[
     (df['value'] >= df['value'].quantile(0.025)) &
     (df['value'] <= df['value'].quantile(0.975))
@@ -23,7 +21,6 @@ def draw_line_plot():
     return fig
 
 def draw_bar_plot():
-    # Prepare data for bar plot
     df_bar = df.copy()
     df_bar['year'] = df_bar.index.year
     df_bar['month'] = df_bar.index.month_name()
@@ -39,7 +36,6 @@ def draw_bar_plot():
     return fig
 
 def draw_box_plot():
-    # Prepare data for box plots
     df_box = df.copy()
     df_box.reset_index(inplace=True)
     df_box['year'] = df_box['date'].dt.year
@@ -47,13 +43,11 @@ def draw_box_plot():
 
     fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 
-    # Year-wise Box Plot (Trend)
     sns.boxplot(x='year', y='value', data=df_box, ax=axes[0])
     axes[0].set_title('Year-wise Box Plot (Trend)')
     axes[0].set_xlabel('Year')
     axes[0].set_ylabel('Page Views')
 
-    # Month-wise Box Plot (Seasonality)
     sns.boxplot(x='month', y='value', data=df_box,
                 order=['January', 'February', 'March', 'April', 'May', 'June',
                        'July', 'August', 'September', 'October', 'November', 'December'], ax=axes[1])
